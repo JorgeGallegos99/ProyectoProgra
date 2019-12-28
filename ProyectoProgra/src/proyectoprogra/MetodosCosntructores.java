a824677c436cb7f6f0837812067e07c81f232ef2
Jgallegos <Jgallegos@Jgallegos99>
2019-12-27 22:21:37 -0500

    v1

---
+++ ProyectoProgra/src/proyectoprogra/MetodosCosntructores.java
@@ -0,0 +1,13 @@
+package proyectoprogra;
+
+/*
+setTitle(""); //pone el titulo
+setLocationRelativeTo(true); //abre la ventana en el centro de la patalla
+setLocation(x,y) posicion 
+setBounds(int x, int y, ancho, alto); mover los objetos
+
+etiquetas de texto
+objeto.setForeground(Color.color)
+objeto.setOpaque(true);  objeto.setBackground(Color.yellow);//color del fondo etiqueta
+
+*/

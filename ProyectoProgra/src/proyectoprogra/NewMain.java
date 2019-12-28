29b073cda72ba20bca91e355cf921cd88b2177a0
Jgallegos <Jgallegos@Jgallegos99>
2019-12-27 22:21:58 -0500

    v1

---
+++ ProyectoProgra/src/proyectoprogra/NewMain.java
@@ -0,0 +1,28 @@
+package proyectoprogra;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author cyber
+ */
+public class NewMain {
+
+    public static void main(String[] args) {
+        
+//        Ventana ventana = new Ventana();
+//        ventana.setResizable(false);
+//        ventana.setVisible(true);
+        
+        Estudiante est = new Estudiante();
+        est.ingresar();
+        System.out.println(est.toString());
+        System.out.println("Nueva version");
+    }
+    
+}
